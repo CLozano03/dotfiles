6944be28f1decc39ee91361cf964b4ca9787671b
CLozano03 <cesar.lozano.argueso@gmail.com>
2023-12-26 20:12:34 +0100

    añadidos nuevos flags y mas aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,23 +1,75 @@
 #!/bin/zsh
 # Path: ~/.config/zsh/aliases.zsh
 
+# Para algunos 'misspelings'
+alias pdw='pwd'
+alias chomd='chmod' 
 
-alias grep='grep --color=auto'
-alias ls="lsd"
-alias pdw="pwd"
+# Configuración de zsh
+alias zshconf='code ~/.zshrc'
+alias ohmyzsh='code ~/.oh-my-zsh'
+# Actualizar configuración de zsh
+alias zshrc='source ~/.zshrc'
+
+alias ls='lsd'
 alias cat='bat --style=plain --color=always'
-alias catn="command cat"
-alias clear="clear; neofetch"
-alias c="clear"
+alias catn='command cat'
+alias c='clear'
+alias v='nvim'
+alias scilab='scilab &'
 
-alias v="nvim"
+# Mostrar NEOFETCH al limpiar la terminal
+alias clear='clear && neofetch'
 
-alias scilab="scilab &"
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
 
-alias zshconfig="code ~/.zshrc"
-alias ohmyzsh="code ~/.oh-my-zsh"
+# cp, mv, rm con verbose y mas seguridad
+alias cp='cp -vi'
+alias mv='mv -vi'
+alias rm='rm -v --preserve-root'
+
+# Añadir flags
+alias mkdir='mkdir -pv'
+alias df='df -h'
+alias du='du -h'
+alias free='free -m'
+alias ping='ping -c 3'
+
+# Comandos de directorios
+alias ..='cd ..'
+alias .2='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Comandos de git
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gca='git commit --amend'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gbr='git branch'
+alias gbrd='git branch -d'
+
+# Actualizar sistema
+alias update='sudo pacman -Syu'
+alias updateyay='yay -Syu'
 
 # Visor de pdf evince con open
 open() { evince "$@" > /dev/null 2>&1 & disown; }
-# Visor de imagenes con view
+# Visor de imagenes  geeqie con view
 view() { geeqie "$@" > /dev/null 2>&1 & disown; }
+
+# Generador de contraseñas
+alias genpasswd='echo `env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 20`'
+
+# Rickroll 😏
+alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
+
+
+
+
