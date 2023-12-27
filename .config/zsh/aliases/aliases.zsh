abbc63e6e39d6c14d2e5a7aa4da2752f3837bccc
CLozano03 <cesar.lozano.argueso@gmail.com>
2023-12-27 13:47:06 +0100

    added: lsd config y algunos aliases

--- .config/zsh/aliases/aliases.zsh
+++ .config/zsh/aliases/aliases.zsh
@@ -12,7 +12,11 @@ alias qtconf='code ~/.config/qtile/config.py'
 # Actualizar configuración de zsh
 alias zshrc='source ~/.zshrc'
 
+# Cambiar ls por lsd y ajustar normales
 alias ls='lsd'
+alias lsn='command ls'
+alias lln='command ls -l'
+
 alias cat='bat --color=always'
 alias catn='command cat'
 alias c='clear'
