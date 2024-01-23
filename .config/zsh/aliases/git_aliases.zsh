2992e7ea70775db41db62db1f7df1e5226db820b
CLozano03 <cesar.lozano.argueso@gmail.com>
2024-01-23 14:33:43 +0100

    git aliases updated

---
+++ .config/zsh/aliases/git_aliases.zsh
@@ -0,0 +1,34 @@
+# Comandos de git
+alias ga="git add"
+alias gaa="git add --all"
+
+alias gb="git branch --sort=committerdate"
+
+alias gco="git checkout"
+
+alias gc="git commit --verbose"
+alias gca="git commit --verbose --all --amend"
+alias gcan="git commit --verbose --all --no-edit --amend"
+
+alias gd="git diff"
+alias gdh="git diff HEAD"
+alias gdca="git diff --cached"
+alias gds="git diff --staged"
+
+alias grb="git rebase"
+alias grba="git rebase --abort"
+alias grbc="git rebase --continue"
+alias grbi="git rebase --interactive"
+
+alias grst="git restore --staged"
+
+alias gsh="git show"
+
+alias gst="git status"
+
+alias gstl="git stash list"
+alias gsta="git stash push"
+alias gstp="git stash pop"
+
+alias gsw="git switch"
+alias gswc="git switch --create"
