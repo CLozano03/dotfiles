fe5f2ba61a567784b303e5152f52a645abaaa3f1
CLozano03 <cesar.lozano.argueso@gmail.com>
2024-06-09 23:23:36 +0200

    updated some hypr scripts

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,32 +1,25 @@
 #!/bin/sh
-#  ____                               _           _    
-# / ___|  ___ _ __ ___  ___ _ __  ___| |__   ___ | |_  
-# \___ \ / __| '__/ _ \/ _ \ '_ \/ __| '_ \ / _ \| __| 
-#  ___) | (__| | |  __/  __/ | | \__ \ | | | (_) | |_  
-# |____/ \___|_|  \___|\___|_| |_|___/_| |_|\___/ \__| 
-#                                                      
-#  
-# by Stephan Raabe (2023) 
-# ----------------------------------------------------- 
 
 arg1=("$1")
 
 DIR="$HOME/Imagenes/screenshots/"
-NAME="screenshot_$(date +%d%m%Y_%H%M%S).png"
+NAME="scs_$(date +%d%m%Y_%H%M%S).png"
+
+if [ ! -d "$DIR" ]; then
+    mkdir -p $DIR
+fi
 
 case $arg1 in
     1)
         grim -g "$(slurp)" "$DIR$NAME"
-        xclip -selection clipboard -t image/png -i "$DIR$NAME"
-        echo "Screenshot created and copied to clipboard" "Mode: Selected area"
+        command cat "$DIR$NAME" | wl-copy
         #notify-send "Screenshot created and copied to clipboard" "Mode: Selected area"
-        swappy -f "$DIR$NAME"
+        swappy  -f "$DIR$NAME"
     ;;
     2)
-        sleep 1
+        sleep 0.6
         grim "$DIR$NAME" 
-        xclip -selection clipboard -t image/png -i "$DIR$NAME"
-        echo "Screenshot created and copied to clipboard" "Mode: Fullscreen"
+        command cat "$DIR$NAME" | wl-copy
         #notify-send "Screenshot created and copied to clipboard" "Mode: Fullscreen"
         swappy -f "$DIR$NAME"
     ;;
