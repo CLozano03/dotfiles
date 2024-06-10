a5c54d85c4eece5b36f47a6d682c1cb8014135b3
CLozano03 <cesar.lozano.argueso@gmail.com>
2024-06-10 18:07:08 +0200

    nvim conf with NVChad

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- Carga de opciones
 require("user")
+return {
+    "nvim-lua/plenary.nvim" -- lua functions that many plugins use
+    -- "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  }
--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,84 +1,42 @@
--- Enable relative line numbers
-vim.opt.nu = true
-vim.opt.rnu = true
+local opt = vim.opt -- for conciseness
 
--- Disable showing the mode below the statusline
-vim.opt.showmode = false
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
--- Set tabs to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
 
--- Enable auto indenting and set it to spaces
-vim.opt.smartindent = true
-vim.opt.shiftwidth = 2
+-- line wrapping
+opt.wrap = false -- disable line wrapping
 
--- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
-vim.opt.breakindent = true
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
--- Enable incremental searching
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
 
--- Disable text wrap
-vim.opt.wrap = false
+-- appearance
 
--- Set leader key to space
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
--- Better splitting
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
--- Enable mouse mode
-vim.opt.mouse = "a"
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
--- Enable ignorecase + smartcase for better searching
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
 
--- Decrease updatetime to 250ms
-vim.opt.updatetime = 250
-
--- Set completeopt to have a better completion experience
-vim.opt.completeopt = { "menuone", "noselect" }
-
--- Enable persistent undo history
-vim.opt.undofile = true
-
--- Enable 24-bit color
-vim.opt.termguicolors = true
-
--- Enable the sign column to prevent the screen from jumping
-vim.opt.signcolumn = "yes"
-
--- Enable access to System Clipboard
-vim.opt.clipboard = "unnamed,unnamedplus"
-
--- Enable cursor line highlight
-vim.opt.cursorline = true
-
--- Set fold settings
--- These options were reccommended by nvim-ufo
--- See: https://github.com/kevinhwang91/nvim-ufo#minimal-configuration
-vim.opt.foldcolumn = "0"
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 99
-vim.opt.foldenable = true
-
--- Always keep 8 lines above/below cursor unless at start/end of file
-vim.opt.scrolloff = 8
-
--- Place a column line
-vim.opt.colorcolumn = "80"
-
-vim.opt.guicursor = {
-	"n-v-c:block", -- Normal, visual, command-line: block cursor
-	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
-	"r-cr:hor20", -- Replace, command-line replace: horizontal bar cursor with 20% height
-	"o:hor50", -- Operator-pending: horizontal bar cursor with 50% height
-	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
-	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
-}
+-- turn off swapfile
+opt.swapfile = false
