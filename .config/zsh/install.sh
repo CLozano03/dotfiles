8169175563adb2126cd32fd604977a51c9ebb6fd
CLozano03 <cesar.lozano.argueso@gmail.com>
2024-06-28 14:18:29 +0200

    install testing correction

--- .config/zsh/install.sh
+++ .config/zsh/install.sh
@@ -18,10 +18,10 @@ if [[ ! -d "$zsh_config_dir" ]]; then
   mkdir -v -p "$zsh_config_dir"
 fi
 
-dirname_install="dotfiles_install"
+dirname_install="/tmp/dotfiles_install"
 
 git clone https://github.com/CLozano03/dotfiles "$dirname_install"
 
-cp "$dirname_install"/.zshrc ~/.zshrc
-cp -r "$dirname_install"/.config/zsh/* "$zsh_config_dir"
-rm -rf "$dirname_install"
+cp -v "$dirname_install"/.zshrc ~/.zshrc
+cp -v -r "$dirname_install"/.config/zsh/* "$zsh_config_dir"
+rm -v -rf "$dirname_install"
