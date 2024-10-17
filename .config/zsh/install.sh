bc378ce84f404c1470596a285b562e07d2f91a49
César <98660262+CLozano03@users.noreply.github.com>
2024-10-17 12:40:51 +0200

    Update install.sh

--- .config/zsh/install.sh
+++ .config/zsh/install.sh
@@ -3,15 +3,14 @@
 sudo pacman -Syu --noconfirm
 
 #Install yay if it is not installed yet
-pacman -Q yay &> /dev/null
+pacman -Q yay &> /dev/null 
 
 if [[ ! $? == 0 ]]; then
-  pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si;
+  sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si;
 else echo "==> Yay instaled"; fi
 
 sudo pacman -S figlet --noconfirm
-clear 
-figlet zsh install && sleep 1
+clear && figlet zsh install && sleep 1
 
 #Installing all stuff
 sudo pacman -S zsh zoxide fzf fastfetch lsd bat --noconfirm
@@ -33,6 +32,6 @@ git clone https://github.com/CLozano03/dotfiles "$dirname_install" -q
 echo -e "  ==>  Copying ~/.zshrc..." && sleep 0.5 && cp "$dirname_install"/.zshrc $HOME/.zshrc
 echo -e "  ==>  Copying zsh config in "$zsh_config_dir"..." && sleep 0.5 && cp -r "$dirname_install"/.config/zsh/* "$zsh_config_dir"
 echo -e "  ==>  Deleting aux directory for instalation" && sleep 0.5 && rm -rf "$dirname_install" 
-echo -e "\n  ==>  Zsh configuration ready for use. Close your terminal and reopen it for installing all the plugins"
+echo -e "\n  ==>  Zsh configuration ready for use. Close your terminal and reopen it for installing all the plugins\n"
 
 exit 0
