c018c42314d17127de29f8caae4faf70b12239c1
CLozano03 <cesar.lozano.argueso@gmail.com>
2024-12-04 09:27:33 +0100

    Updated hypr Conf

--- .config/hypr/scripts/lock.sh
+++ .config/hypr/scripts/lock.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Pause first if there is media playing
+if [[ "$(playerctl status)" == "playing" ]]; then 
+    sh ~/.config/hypr/scripts/media.sh --pause && sleep 0.2
+fi
+
 pidof hyprlock || hyprlock -q 
--- .config/hypr/scripts/reload.sh
+++ .config/hypr/scripts/reload.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-sh ~/.config/hypr/scripts/dunst.sh      # dunst
-hyprctl reload && notify-send "Hypr reload"            # hypr  
-sh "$HOME"/.config/waybar/launch.sh    && notify-send "Waybar restarted"   # waybar
-sh ~/.config/hypr/scripts/term_ack.sh   # term_ack
+# Kill processes to be restarted
+_ps=(waybar rofi swaync hypridle)
+for _prs in "${_ps[@]}"; do
+    if pidof "${_prs}" >/dev/null; then
+        pkill "${_prs}"
+    fi
+done
+
+# Sway
+# sleep 0.2
+# swaync
+# notify-send "Sway reload"
+
+# Hypr
+hyprctl reload && notify-send "Hypr reload"
+hypridle &
+
+# Waybar
+waybar &
+notify-send "Waybar restarted"
