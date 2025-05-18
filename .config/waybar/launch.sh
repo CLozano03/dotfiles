7b9addd3bef520fe71aca3b407235b59856d17f6
CLozano03 <cesar.lozano.argueso@gmail.com>
2025-05-18 18:39:56 +0200

    refactor: waybar dir

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -1,6 +1,20 @@
-#!/bin/bash
-# Launch waybar
+#!/bin/sh
 
-waybar &
+# Kill all waybar instances
+killall waybar
+pkill waybar
+sleep 0.3
+
+WAYBAR_DIR="$HOME/.config/waybar"
+current_theme="cl_original"
+config_file="config.jsonc"
+style_file="style.css"
+
+if [ ! -f $HOME/.cache/waybar-disabled ]; then
+    waybar -c ${WAYBAR_DIR}/themes/${current_theme}/$config_file -s ${WAYBAR_DIR}/themes/${current_theme}/$style_file &
+else
+    echo ":: Waybar disabled"
+    notify-send "! Waybar disabled"
+fi
 
 exit 0
