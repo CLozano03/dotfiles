5bac57fa7df4b19047dee6301ba0c858833355dd
CLozano03 <cesar.lozano.argueso@gmail.com>
2025-06-22 23:29:19 +0200

    feat: join right in cl style

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 WAYBAR_DIR="$HOME/.config/waybar"
-current_theme="cl-original"
 config_file="config.jsonc"
 style_file="style.css"
+
+current_theme="cl-original"
 
 # Kill all waybar instances
 killall waybar
 pkill waybar
-sleep 0.3
+# sleep 0.2
 
 if [ ! -f $HOME/.cache/waybar-disabled ]; then
     waybar -c ${WAYBAR_DIR}/themes/${current_theme}/$config_file -s ${WAYBAR_DIR}/themes/${current_theme}/$style_file &
