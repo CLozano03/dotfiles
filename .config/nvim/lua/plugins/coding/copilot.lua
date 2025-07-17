8b2497090cbfc37503ec34e89c6a547068dfcbfb
César Lozano <clozarch@proton.me>
2025-07-17 18:47:41 +0200

    feat: toggle copilot function

--- .config/nvim/lua/plugins/coding/copilot.lua
+++ .config/nvim/lua/plugins/coding/copilot.lua
@@ -1,15 +1,35 @@
 return {
-  -- { 'github/copilot.vim' },
-  -- {
-  --   'CopilotC-Nvim/CopilotChat.nvim',
-  --   dependencies = {
-  --     { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
-  --     { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
-  --   },
-  --   build = 'make tiktoken', -- Only on MacOS or Linux
-  --   opts = {
-  --     -- See Configuration section for options
-  --   },
-  --   -- See Commands section for default commands if you want to lazy load on them
-  -- },
+   {
+      'github/copilot.vim',
+      enabled = true,
+      config = function()
+         vim.g.copilot_is_enabled = true
+
+         local function toggle_copilot_status()
+            if vim.g.copilot_is_enabled then
+               vim.cmd 'Copilot disable'
+               print 'Copilot disabled.'
+               vim.g.copilot_is_enabled = false
+            else
+               vim.cmd 'Copilot enable'
+               print 'Copilot enabled.'
+               vim.g.copilot_is_enabled = true
+            end
+         end
+
+         vim.keymap.set('n', '<leader>cg', toggle_copilot_status, { desc = 'Toggle GitHub Copilot' })
+      end,
+   },
+   -- {
+   --   'CopilotC-Nvim/CopilotChat.nvim',
+   --   dependencies = {
+   --     { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+   --     { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+   --   },
+   --   build = 'make tiktoken', -- Only on MacOS or Linux
+   --   opts = {
+   --     -- See Configuration section for options
+   --   },
+   --   -- See Commands section for default commands if you want to lazy load on them
+   -- },
 }
