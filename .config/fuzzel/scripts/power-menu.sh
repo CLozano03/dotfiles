c975cad7cd2bb5b1898c32cf795e589a05975996
César Lozano <clozarch@proton.me>
2025-11-03 15:16:54 +0100

    feat: added fuzzel

---
+++ .config/fuzzel/scripts/power-menu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SELECTION="$(printf "󰐥 Shutdown\n󰌾 Lock\n󰤄 Suspend\n󰍃 Log out\n Reboot\n Reboot to UEFI" | fuzzel --dmenu)"
+
+}
+
+case $SELECTION in
+*"󰌾 Lock"*)
+    hyprlock -q
+    ;;
+*"󰤄 Suspend"*)
+    systemctl suspend
+    ;;
+*"󰍃 Log out"*)
+    hyprctl dispatch exit
+    ;;
+*" Reboot"*)
+    systemctl reboot
+    ;;
+*" Reboot to UEFI"*)
+    systemctl reboot --firmware-setup
+    ;;
+*"󰐥 Shutdown"*)
+    shutdown now
+    ;;
+esac
