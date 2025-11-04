677f2907d665976476a5f85729b26faa92a2e4fa
César Lozano <clozarch@proton.me>
2025-11-04 17:36:51 +0100

    refactor: DE workflow

--- .config/hypr/scripts/exit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sleep 0.5
-
-hyprctl dispatch exit
--- .config/hypr/scripts/workspace_action.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-BLOCK_SIZE=10
-CURRENT_WS=$(hyprctl activeworkspace -j | jq -r .id)
-BASE=$(( ( (CURRENT_WS - 1) / BLOCK_SIZE ) * BLOCK_SIZE ))
-TARGET_WS=$(( BASE + $2 ))
-
-hyprctl dispatch "$1" "$TARGET_WS"
