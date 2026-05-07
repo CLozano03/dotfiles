1483d0c98bf76389eceac318c35ec5c8e1368418
CLozano03 <clozarch@proton.me>
2026-05-07 20:51:36 +0200

    fix: coding issues tressitter

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -111,42 +111,43 @@ return {
       },
       ft = { 'html', 'css', 'javascript', 'typescript' },
    },
-   { 'nvim-treesitter/nvim-treesitter',
-    branch = 'main',
-    init = function()
-      vim.g.loaded_nvim_treesitter = 1
-    end,
-  },
-
-  { 'lewis6991/ts-install.nvim',
-    -- OPTIONAL
-    config = function()
-      require('ts-install').setup({
-        ensure_install = {
-          'lua',
-          'c',
-          'bash',
-          'python',
-          'latex',
-          -- etc
-        },
-      })
-      vim.api.nvim_create_autocmd("FileType", {
-        desc = "Enable Treesitter",
-        group = vim.api.nvim_create_augroup("enable_treesitter", {}),
-        -- Don't filter by `pattern`, install and enable Treesitter parsers for all languages.
-        callback = function()
-          -- Enable Treesitter syntax highlighting.
-          if pcall(vim.treesitter.start) then
-            -- Use Treesitter indentation and folds.
-            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-            -- vim.wo.foldmethod = "expr"
-            -- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-          end
+   {
+      'nvim-treesitter/nvim-treesitter',
+      branch = 'main',
+      init = function()
+         vim.g.loaded_nvim_treesitter = 1
       end,
-      })
-    end
-  },
+   },
+
+   {
+      'lewis6991/ts-install.nvim',
+      -- OPTIONAL
+      config = function()
+         require('ts-install').setup {
+            ensure_install = {
+               'lua',
+               'c',
+               'bash',
+               'python',
+               -- etc
+            },
+         }
+         vim.api.nvim_create_autocmd('FileType', {
+            desc = 'Enable Treesitter',
+            group = vim.api.nvim_create_augroup('enable_treesitter', {}),
+            -- Don't filter by `pattern`, install and enable Treesitter parsers for all languages.
+            callback = function()
+               -- Enable Treesitter syntax highlighting.
+               if pcall(vim.treesitter.start) then
+                  -- Use Treesitter indentation and folds.
+                  vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                  -- vim.wo.foldmethod = "expr"
+                  -- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+               end
+            end,
+         })
+      end,
+   },
    -- { -- Highlight, edit, and navigate code
    --    'nvim-treesitter/nvim-treesitter',
    --    branch = 'main',
